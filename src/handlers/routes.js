fb8786fa5a98e07281f15628641bb3a9c5ffdaea
Dmitry Nikulin <dmitr-nikulin@mail.ru>
2016-11-19 15:26:37 +0300

    New API

--- src/handlers/routes.js
+++ src/handlers/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let BaseHandler = require('./baseHandler');
+const picBaseUrl = 'http://54.186.218.70:8000/pics/';
 
 
 class RoutesListHandler extends BaseHandler {
@@ -10,25 +11,84 @@ class RoutesListHandler extends BaseHandler {
   }
 
   handlerFunction (req) {
-    let categoryIds = [];
+    let where;
     if(req.query.categories) {
-      categoryIds = req.query.categories.split(',')
-        .map(parseInt).filter(id => !isNaN(id));
+      where = {
+        CategoryId: req.query.categories
+          .split(',')
+          .map(parseInt)
+          .filter(id => !isNaN(id))
+      };
+    }
+
+    let categories = {};
+    let pictures = {};
+    let routes = {};
+    let points = {};
+
+    function arr(x){
+      return Object.keys(x).map(k => x[k]);
     }
 
     return this.database.models.Route.findAll({
       include: [
+        { all: true },
         {
           model: this.database.models.Category,
-          where: {
-            id: categoryIds
-          }
+          where
         }
       ]
     }).then(result => {
       // Приводим объекты к чистому JSON (без методов/свойств от sequelize)
+      result.forEach(result => {
+        result = result.get({ plain: true });
+
+        let catIds = result.Categories.map(category => {
+          categories[category.id] = {
+            id: category.id,
+            name: category.name
+          };
+          return category.id;
+        });
+
+        let pointIds = result.Points.map(point => {
+          points[point.id] = {
+            id: point.id,
+            name: point.name,
+            coordinates: point.latitude+','+point.longitude,
+            time: point.time,
+            pinPicture: point.PinPictureId
+          };
+          return point.id;
+        });
+
+        let picIds = result.Pictures.map(picture => {
+          pictures[picture.id] = {
+            id: picture.id,
+            url: picBaseUrl + picture.filename
+          };
+          return picture.id;
+        });
+
+        routes[result.id] = {
+          id: result.id,
+          name: result.name,
+          description: result.description,
+          rating: result.rating,
+          duration: result.duration,
+          price: result.price,
+          points: pointIds,
+          pictures: picIds,
+          categories: catIds,
+          cover: result.Cover.id
+        };
+      });
+
       return {
-        routes: result.map(c => c.get({ plain: true }))
+        routes: arr(routes),
+        pictures: arr(pictures),
+        categories: arr(categories),
+        points: arr(points)
       };
     });
   }
